4d612edb9aa7788f9258d8e8d9160c02eca83f98
Mai Thanh Minh <thanhminh.mr@gmail.com>
2026-01-27 18:51:11 +0700

    Fixing UB in `Bytes::try_unsplit` by saving original pointer when `Bytes::from_static`

--- src/bytes.rs
+++ src/bytes.rs
@@ -172,7 +172,7 @@ impl Bytes {
         Bytes {
             ptr: bytes.as_ptr(),
             len: bytes.len(),
-            data: AtomicPtr::new(ptr::null_mut()),
+            data: AtomicPtr::new(bytes.as_ptr() as *mut ()),
             vtable: &STATIC_VTABLE,
         }
     }
@@ -183,7 +183,7 @@ impl Bytes {
         Bytes {
             ptr: bytes.as_ptr(),
             len: bytes.len(),
-            data: AtomicPtr::new(ptr::null_mut()),
+            data: AtomicPtr::new(bytes.as_ptr() as *mut ()),
             vtable: &STATIC_VTABLE,
         }
     }
@@ -199,7 +199,7 @@ impl Bytes {
         Bytes {
             ptr,
             len: 0,
-            data: AtomicPtr::new(ptr::null_mut()),
+            data: AtomicPtr::new(ptr as *mut ()),
             vtable: &STATIC_VTABLE,
         }
     }
@@ -1173,9 +1173,9 @@ const STATIC_VTABLE: Vtable = Vtable {
     drop: static_drop,
 };
 
-unsafe fn static_clone(_: &AtomicPtr<()>, ptr: *const u8, len: usize) -> Bytes {
-    let slice = slice::from_raw_parts(ptr, len);
-    Bytes::from_static(slice)
+unsafe fn static_clone(data: &AtomicPtr<()>, ptr: *const u8, len: usize) -> Bytes {
+    let data = AtomicPtr::new(data.load(Ordering::Relaxed));
+    Bytes::with_vtable(ptr, len, data, &STATIC_VTABLE)
 }
 
 unsafe fn static_to_vec(_: *mut (), ptr: *const u8, len: usize) -> Vec<u8> {
